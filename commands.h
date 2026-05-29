41b056b7835806c2a0e4428f8366ac90323bf9e2
Marco Mese <mese@na.infn.it>
2026-05-29 14:37:24 +0000

    Changed command codification

--- commands.h
+++ commands.h
@@ -16,45 +16,44 @@
 #include <sys/types.h>
 #include "registers.h"
 
-#define NONE            0x00
-
-#define START_RUN       0x02
-#define STOP_RUN        0x03
-#define RELEASE_BUSY    0x04
-#define SET_BUSY        0x05
-#define TRIGGER         0x06
-#define RESET_GPS       0x07
-#define CONFIGURE_GPS   0x08
-#define GPS1_ON         0x09
-#define GPS2_ON         0x0A
-#define RESET_GTU_COUNT 0x0B
-#define RESET_PACKET_NR 0x0C
-#define RESET_TRG_COUNT 0x0D
-#define RESET_ALL_COUNT 0x0E
-#define PPS_TRG_ON      0x0F
-#define PPS_TRG_OFF     0x10
-#define MASK_EXT_TRG    0x11
-#define UNMASK_EXT_TRG  0x12
-#define SELF_TRG        0x13
-#define SELF_TRG_OFF    0x14
-#define NO_ZYNQ1        0x15
-#define NO_ZYNQ2        0x16
-#define NO_ZYNQ3        0x17
-#define ZYNQ1_ON        0x18
-#define ZYNQ2_ON        0x19
-#define ZYNQ3_ON        0x1A
-#define READ_STATUS     0x1B
-#define READ_GTUCOUNTER 0x1C
-#define READ_TRGCOUNTER 0x1D
-#define READ_L11COUNTER 0x1E
-#define READ_L12COUNTER 0x1F
-#define READ_L13COUNTER 0x20
-#define NO_GPS1         0x21
-#define NO_GPS2         0x22
-#define GPSAUTO_ON      0x23
-#define GPSAUTO_NO      0x24
-
-#define EXIT            0xFF
+#define NONE             0x00000000
+#define START_RUN        0xFFFFFFFF
+#define STOP_RUN         0xAAAAAAAA
+#define RELEASE_BUSY     0x55555555
+#define SET_BUSY         0xCCCCCCCC
+#define TRIGGER          0x33333333
+#define RESET_GPS        0x66666666
+#define CONFIGURE_GPS    0x99999999
+#define GPS1_ON          0xF0F0F0F0
+#define GPS2_ON          0x0F0F0F0F
+#define RESET_GTU_COUNT  0x5A5A5A5A
+#define RESET_TRG_COUNT  0xA5A5A5A5
+#define RESET_PCK_COUNT  0x3C3C3C3C
+#define RESET_ALL_COUNT  0xC3C3C3C3
+#define PPS_TRG_ON       0x96969696
+#define PPS_TRG_OFF      0x69696969
+#define MASK_EXT_TRG     0xFF00FF00
+#define UNMASK_EXT_TRG   0x00FF00FF
+#define SELF_TRG_ON      0x55AA55AA
+#define SELF_TRG_OFF     0xAA55AA55
+#define NO_ZYNQ0         0x33CC33CC
+#define NO_ZYNQ1         0xCC33CC33
+#define NO_ZYNQ2         0x99669966
+#define NO_ZYNQ3         0x66996699
+#define ZYNQ0_ON         0x0FF00FF0
+#define ZYNQ1_ON         0xF00FF00F
+#define ZYNQ2_ON         0xA55AA55A
+#define ZYNQ3_ON         0x5AA55AA5
+#define GPS1_NO          0xC33CC33C
+#define GPS2_NO          0x3CC33CC3
+#define GPS_AUTO_ON      0x69966996
+#define GPS_AUTO_NO      0x96699669
+#define READ_GTUCOUNTER  0xFFFF0000
+#define READ_TRGCOUNTER  0x5555AAAA
+#define READ_L11COUNTER  0xAAAA5555
+#define READ_L12COUNTER  0x3333CCCC
+#define READ_L13COUNTER  0xCCCC3333
+#define EXIT             0x0000FFFF
 
 #define CMD_MAX_LEN     15
 
