f491408e040f5d0baa1479ac7753ebb5220a15a3
Marco Mese <mese@na.infn.it>
2026-07-03 15:28:00 +0200

    fixed missing pointer

--- main.h
+++ main.h
@@ -108,7 +108,7 @@ typedef struct chkFifoArgs{
     volatile uint32_t* fifoData;
     char*            gpsStr;
     int              fdTrg;
-    uint32_t         header;
+    uint32_t*        header;
     pthread_mutex_t* mtx;
     imuShared_t*     imuShared;
     imuThreadArgs_t* imuArgs;
